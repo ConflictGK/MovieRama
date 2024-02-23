cdd5fc7b1e42f1c144e34c08ae223d471faacca3
g.karagiannopoulos <g.karagiannopoulos@wind.gr>
2024-02-23 21:25:33 +0200

    Adding switch in view between logged in and not logged in state

--- src/main/java/org/workable/movierama/controller/MovieController.java
+++ src/main/java/org/workable/movierama/controller/MovieController.java
@@ -1,6 +1,8 @@
 package org.workable.movierama.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,17 +10,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.workable.movierama.model.Movie;
+import org.workable.movierama.model.User;
 import org.workable.movierama.service.MovieService;
+import org.workable.movierama.service.UserService;
+
+import java.time.LocalDate;
 
 @Controller
 @RequiredArgsConstructor
 public class MovieController {
 
     private final MovieService movieService;
+    private final UserService userService;
 
     @GetMapping("/movies")
-    public String listMovies(Model model) {
+    public String listMovies(Model model, @AuthenticationPrincipal UserDetails currentUser) {
         model.addAttribute("movies", movieService.findAll());
+        if (currentUser != null) {
+            model.addAttribute("username", currentUser.getUsername());
+        }
         return "movies";
     }
 
@@ -29,10 +39,18 @@ public class MovieController {
     }
 
     @PostMapping("/movies")
-    public String handleMovieRegistration(@ModelAttribute("movie") Movie movie, BindingResult bindingResult) {
+    public String handleMovieRegistration(@ModelAttribute("movie") Movie movie,
+                                          BindingResult bindingResult,
+                                          @AuthenticationPrincipal UserDetails userDetails) {
+
         if (bindingResult.hasErrors()) {
             return "registerMovie";
         }
+
+        User user = userService.findUserByUsername(userDetails.getUsername());
+        movie.setUser(user);
+        movie.setDateAdded(LocalDate.now());
+
         movieService.save(movie);
         return "redirect:/movies";
     }
