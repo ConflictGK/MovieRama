9132b6b98e7e0cea8be48721112dd8c2a5c036f4
g.karagiannopoulos <g.karagiannopoulos@wind.gr>
2024-02-24 15:21:56 +0200

    Impl of UserDetailsService to validate users from DB.

--- src/main/java/org/workable/movierama/service/impl/UserServiceImpl.java
+++ src/main/java/org/workable/movierama/service/impl/UserServiceImpl.java
@@ -3,15 +3,20 @@ package org.workable.movierama.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.workable.movierama.model.User;
 import org.workable.movierama.repository.UserRepository;
 import org.workable.movierama.service.UserService;
 
+import java.util.ArrayList;
+
 @RequiredArgsConstructor
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
@@ -33,4 +38,13 @@ public class UserServiceImpl implements UserService {
         return userRepository.findByUsername(username);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
 }
